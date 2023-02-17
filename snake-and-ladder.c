d0308bd91c065a5703d38f56bd561d93db534bd1
Kavyashah26 <125577508+Kavyashah26@users.noreply.github.com>
2023-02-17 22:26:18 +0530

    added colour for each user and mixed users also
    user1 == yellow 
    user2 == blue
    merged user == green background

--- snake-and-ladder.c
+++ snake-and-ladder.c
@@ -16,37 +16,13 @@
 #define BMAG "\e[1;35m"
 #define BCYN "\e[1;36m"
 #define BWHT "\e[1;37m"
-int check(int num)
-{
-    switch(num){
-        case 99:
-        return 2;
-        break;
-        case 85:
-        return 55;
-        break;
-        case 68:
-        return 32;
-        break;
-        case 80:
-        return 98;
-        break;
-        case 45:
-        return 63;
-        break;
-        case 71:
-        return 90;
-        break;
-        case 8:
-        return 30;
-        break;
-        case 21:
-        return 41;
-        break;
-        default:
-        return num;
-}
-}
+
+#define YELB "\e[43m"
+#define BLUB "\e[44m"
+#define GRNB "\e[42m"
+
+
+#define HRED "\e[0;91m"
 void printnum_ladder(int num)
 {
     printf(MAG " %3d " WHT, num);
@@ -56,11 +32,26 @@ void printnum_snake(int num)
     if (num == 99 || num == 85 || num == 68)
         printf(BRED " %3d " WHT, num);
     else
-        printf(RED " %3d " WHT, num);
+        printf(HRED " %3d " WHT, num);
 }
-
-void display(int arr[][10])
+void print_user_n1(int num)
 {
+    printf(" ");
+    printf(YELB "%3d " WHT, num);
+}
+void print_user_n2(int num)
+{
+    printf(" ");
+    printf(BLUB "%3d " WHT, num);
+}
+void print_user_equal(int num)
+{
+    printf(" ");
+    printf(GRNB "%3d " WHT, num);
+}
+void display(int arr[][10],int no1,int no2)
+{
+    int flag=1;
     for (int i = 9; i >= 0; i--)
     {
 
@@ -69,12 +60,28 @@ void display(int arr[][10])
             for (int j = 9; j >= 0; j--)
             {
                 int x = arr[i][j];
-                if (x == 8 || x == 12 || x == 30 || x == 21 || x == 45 || x == 40 || x == 80 || x == 82 || x == 98 || x == 57 || x == 63 || x == 71 || x == 90 || x == 41)
-                    printnum_ladder(x);
-                else if (x == 99 || x == 83 || x == 78 || x == 62 || x == 59 || x == 42 || x == 38 || x == 22 || x == 19 || x == 2 || x == 55 || x == 66 || x == 76 || x == 85 || x == 32 || x == 50 || x == 52 || x == 68)
-                    printnum_snake(x);
+                if(no1==no2 && x==no1 )
+                {
+                   flag=0;
+                   print_user_equal(no1);
+                }
+                else if(x==no1)
+                {
+                    print_user_n1(no1);
+                }
+                else if(x==no2)
+                {
+                    print_user_n2(no2);
+                }
                 else
-                    printf(" %3d ", x);
+                {
+                    if (x == 8 || x == 12 || x == 30 || x == 21 || x == 45 || x == 40 || x == 80 || x == 82 || x == 98 || x == 57 || x == 63 || x == 71 || x == 90 || x == 41)
+                        printnum_ladder(x);
+                    else if (x == 99 || x == 83 || x == 78 || x == 62 || x == 59 || x == 42 || x == 38 || x == 22 || x == 19 || x == 2 || x == 55 || x == 66 || x == 76 || x == 85 || x == 32 || x == 50 || x == 52 || x == 68)
+                        printnum_snake(x);
+                    else
+                        printf(" %3d ", x);
+                }
             }
             printf("\n\n");
         }
@@ -82,13 +89,29 @@ void display(int arr[][10])
         {
             for (int j = 0; j < 10; j++)
             {
-                int x = arr[i][j];
-                if (x == 8 || x == 12 || x == 30 || x == 21 || x == 45 || x == 40 || x == 80 || x == 82 || x == 98 || x == 57 || x == 63 || x == 71 || x == 90 || x == 41)
-                    printnum_ladder(x);
-                else if (x == 99 || x == 83 || x == 78 || x == 62 || x == 59 || x == 42 || x == 38 || x == 22 || x == 19 || x == 2 || x == 55 || x == 66 || x == 76 || x == 85 || x == 32 || x == 50 || x == 52 || x == 68)
-                    printnum_snake(x);
+               int x = arr[i][j];
+               if(no1==no2 && x==no1 )
+               {
+                   flag=0;
+                   print_user_equal(no1);
+               }
+                else  if(x==no1)
+                {
+                    print_user_n1(no1);
+                }
+                else if(x==no2)
+                {
+                    print_user_n2(no2);
+                }
                 else
-                    printf(" %3d ", x);
+                {
+                    if (x == 8 || x == 12 || x == 30 || x == 21 || x == 45 || x == 40 || x == 80 || x == 82 || x == 98 || x == 57 || x == 63 || x == 71 || x == 90 || x == 41)
+                        printnum_ladder(x);
+                    else if (x == 99 || x == 83 || x == 78 || x == 62 || x == 59 || x == 42 || x == 38 || x == 22 || x == 19 || x == 2 || x == 55 || x == 66 || x == 76 || x == 85 || x == 32 || x == 50 || x == 52 || x == 68)
+                        printnum_snake(x);
+                    else
+                        printf(" %3d ", x);
+                }
             }
             printf("\n\n");
         }
@@ -103,7 +126,10 @@ int main()
         *(*arr + i) = k++;
     }
     printf("\n\n");
-    display(arr);
+    int prev_no_p1=1;
+    int prev_no_p2=1;
+    display(arr,prev_no_p1,prev_no_p2);
     printf("\n");
-return 0;
+
+    return 0;
 }
