6d6585c510235efb0a172d0c1a8c5cd690d2081b
Nisarg <amlaninisarg15@gmail.com>
2023-02-17 10:25:19 +0530

    added sudoku  9*9 ans

--- sudoku.c
+++ sudoku.c
@@ -1,24 +1,20 @@
+
 #include <stdio.h>
 #define NORMAL  "\x1b[0m"
 #define RED    "\x1b[31m"
 #define GREEN  "\x1b[32m"
-void disp(int *question,int *answer)
+void disp(int *ar)  //? diaplay the output at every step 
 {
     for(int i=1;i<=81;i++)
     {
-        int n=*(answer+i-1);
+        int n=*(ar+i-1);
         if(n!=0)
-        {
-            if(*(question+i)!=0)
-            printf(GREEN"%d ",n);
-            else
-            printf(NORMAL"%d ",n);
-        }
+        printf(GREEN"%d ",n);
         else
         printf(NORMAL"%d ",n);
         
         if(i%3==0 && i%9!=0)
-        printf(NORMAL"|\t");
+        printf("|\t");
         
         if(i%9==0 && i!=81)
         printf("\n");
@@ -27,47 +23,34 @@ void disp(int *question,int *answer)
         printf(NORMAL"_____________________\n");
     }
 }
-void chek(int *question,int *answer,int index)
+void answer(int *ar,int *ans) //? converts ar to answer
 {
-    int row=(index-1)/9;
-    int col=index%9;
-    
-    printf("\nrow=%d\ncol=%d\nprinting row:\n",row,col);
-    for(int i=0;i<9;i++)
-    printf("%d ",*(question+(row*9+i)));
-    
-    printf("\nprinting col:\n");
-    for(int i=0;i<81;i+=9)
-    printf("%d ",*(question+(col+i)));
-
-    printf("\nprinting box::\n");
-    for(int i=0;i<21;i++)
+    for (int i = 0; i < 81; i++)
     {
+        *(ans + i) = *(ar + i); 
         
     }
     
 }
+
+
 int main()
 {
-    int question[27][3]={{0,0,0},{0,0,6},{0,0,3},
-                         {0,0,0},{0,1,0},{4,0,0},
-                         {0,4,0},{8,0,0},{0,7,9},
-                         {8,0,9},{0,7,0},{0,6,0},
-                         {0,6,0},{0,8,0},{0,0,5},
-                         {0,5,7},{0,3,0},{8,0,0},
-                         {2,0,0},{1,0,9},{0,0,0},
-                         {0,0,0},{5,0,3},{0,0,0},
-                         {0,0,0},{0,0,0},{6,0,0}};
+    int ar[27][3]={{0,0,0},{0,0,6},{0,0,3},
+                   {0,0,0},{0,1,0},{4,0,0},
+                   {0,4,0},{8,0,0},{0,7,9},
+                   {8,0,9},{0,7,0},{0,6,0},
+                   {0,6,0},{0,8,0},{0,0,5},
+                   {0,5,7},{0,3,0},{8,0,0},
+                   {2,0,0},{1,0,9},{0,0,0},
+                   {0,0,0},{5,0,3},{0,0,0},
+                   {0,0,0},{0,0,0},{6,0,0}};
+    disp((*ar));
+    printf("\n");
+    printf("\n");
+
+    int ans[9][9];
+    answer(*ar,*ans);
     
-    int answer[9][9];
-    for(int i=0;i<81;i++)
-    *(answer+i)=*(*question+i);
-    
-    int numi[9]={0};
-    for(int i=0;i<81;i++)
-    numi[*(*question+i)-1];
-    
-    disp(*question,answer);
-    chek(*question,answer,15);
-    return 0;
+   return 0;
 }
