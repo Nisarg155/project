cfec9ba52cc1fd8bb5387f6bbefb5ae64f4f6a4d
smitshah084 <118046777+smitshah084@users.noreply.github.com>
2023-02-17 12:01:38 +0530

    display is updated and check is added and position
    display is changed check function is added and position function is added

--- sudoku.c
+++ sudoku.c
@@ -1,56 +1,134 @@
-
 #include <stdio.h>
 #define NORMAL  "\x1b[0m"
 #define RED    "\x1b[31m"
 #define GREEN  "\x1b[32m"
-void disp(int *ar)  //? diaplay the output at every step 
+void disp(int *question,int *answer)
 {
     for(int i=1;i<=81;i++)
     {
-        int n=*(ar+i-1);
+        int n=*(answer+i-1);
         if(n!=0)
-        printf(GREEN"%d ",n);
+        {
+            if(*(question+i-1)!=0)
+            printf(GREEN"%d ",n);
+            else
+            printf(NORMAL"%d ",n);
+        }
         else
-        printf(NORMAL"%d ",n);
+        printf(RED"%d ",n);
         
         if(i%3==0 && i%9!=0)
-        printf("|\t");
+        printf(NORMAL"|\t");
         
         if(i%9==0 && i!=81)
         printf("\n");
         
         if(i%27==0 && i!=81)
-        printf(NORMAL"_____________________\n");
+        printf(NORMAL"_______\n");
     }
 }
-void answer(int *ar,int *ans) //? converts ar to answer
+int chek(int *answer,int num,int index)
 {
-    for (int i = 0; i < 81; i++)
+    int row=(index-1)/9;
+    int col=index%9;
+    
+    //printf("\n%d\nrow=%d\ncol=%d\nprinting row:\n",*(answer+index),row,col);
+    for(int i=0;i<9;i++)
     {
-        *(ans + i) = *(ar + i); 
+    //printf("%d ",*(answer+(row*9+i)));
+    if(*(answer+(row*9+i))==num)
+    return 1;
+    }
+    
+    printf("\nprinting col:\n");
+    for(int i=0;i<81;i+=9)
+    {
+        //printf("%d ",*(answer+(col+i)));
+        if(*(answer+(col+i))==num)
+        return 1;
+    }
+    
+    printf("\nprinting box::\n");
+    row-=row%3;
+    col-=col%3;
+    int ibox=(row*9)+col;
+    printf("\n%d\n",ibox);
+    for(int i=1;i<=21;)
+    {
+        //printf("%d ",*(answer+ibox+i));
+        //printf("%d ",*(answer+ibox+i-1));
+        if(*(answer+ibox+i-1)==num)
+        return 1;
+        if(i%3==0)
+        i+=6;
+        i++;
+    }
+    return 0;
+}
+void position(int (*arr)[9],int (*pos)[18])
+{
+    int k = 0;
+    for (int num = 1; num <10 ; num++)
+    {
+        k=0;
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (num == arr[row][col])
+                {
+                    pos[num-1][k] = row;
+                    pos[num-1][k+1] = col;
+                    k+=2;
+                }
+                
+            }
+            
+        }
         
     }
     
 }
-
-
 int main()
 {
-    int ar[27][3]={{0,0,0},{0,0,6},{0,0,3},
-                   {0,0,0},{0,1,0},{4,0,0},
-                   {0,4,0},{8,0,0},{0,7,9},
-                   {8,0,9},{0,7,0},{0,6,0},
-                   {0,6,0},{0,8,0},{0,0,5},
-                   {0,5,7},{0,3,0},{8,0,0},
-                   {2,0,0},{1,0,9},{0,0,0},
-                   {0,0,0},{5,0,3},{0,0,0},
-                   {0,0,0},{0,0,0},{6,0,0}};
-    disp((*ar));
-    printf("\n");
-    printf("\n");
-
-    int ans[9][9];
-    answer(*ar,*ans);
+    int question[27][3]={{0,0,0},{0,0,6},{0,0,3},
+                         {0,0,0},{0,1,0},{4,0,0},
+                         {0,4,0},{8,0,0},{0,7,9},
+                         {8,0,9},{0,7,0},{0,6,0},
+                         {0,6,0},{0,8,0},{0,0,5},
+                         {0,5,7},{0,3,0},{8,0,0},
+                         {2,0,0},{1,0,9},{0,0,0},
+                         {0,0,0},{5,0,3},{0,0,0},
+                         {0,0,0},{0,0,0},{6,0,0}};
     
-   return 0;
+    int answer[9][9];
+    for(int i=0;i<81;i++)
+    *(*answer+i)=*(*question+i);
+    
+    disp(*question,*answer);
+    printf("\n\n");
+    // printf("%d",chek(*answer,7,49));
+    
+    int ans[9][9],pos[9][18] = {0};
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 18; j++)
+        {
+            pos[i][j]=-1;
+        }
+        
+    }
+    position(question,pos);
+    
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 18; j++)
+        {
+            printf("%d  ",pos[i][j]);
+        }
+        
+        printf("\n");
+        
+    }
+    return 0;
 }
